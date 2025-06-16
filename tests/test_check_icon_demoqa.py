f6855b0d2e602c12815a20d60dd093c021023955
Polina <polina.panfilowa15@mail.ru>
2025-06-16 14:49:22 +0300

    implementation of Page Object pattern

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -5,8 +5,6 @@ import time
 def test_icon_exist(browser):
     demo_qa_page = DemoQa(browser)
     demo_qa_page.visit()
-    time.sleep(3)
-    demo_qa_page.click_on_the_icon()
-    time.sleep(3)
+    demo_qa_page.icon.click()
     assert demo_qa_page.equal_url()
-    assert demo_qa_page.exist_icon()
+    assert demo_qa_page.icon.exist()
